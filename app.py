ab2bb07621b2e3de84e6be5191385200a4f1dcb2
Narotam15102005 <chaubeynarotam@gmail.com>
2026-08-08 22:58:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+from flask import Flask, render_template, jsonify
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/simulate")
+def simulate():
+    with open("scenarios/sample_scenario.json") as f:
+        scenario = json.load(f)
+    return jsonify({"status": "Disaster simulated", "scenario": scenario})
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
